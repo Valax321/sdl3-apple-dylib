7d8d2a353fa9348d6169d872e2c6aa150bfeea8c
Audrey Castillo <valax321@gmail.com>
2026-02-25 11:54:33 +1100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ $sdlVersion = '3.4.2'
 
 &gh repo clone 'libsdl-org/SDL' src/SDL3 -- -b "release-$sdlVersion" --recursive
 
-&cmake -S src/SDL3 -B build/SDL3 -G "Ninja Multi-Config" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13
+&cmake -S src/SDL3 -B build/SDL3 -G "Ninja Multi-Config" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.13"
 &cmake --build build/SDL3 --config Release --target SDL3-shared
 
 New-Item -ItemType "Directory" -Name "artifacts"
