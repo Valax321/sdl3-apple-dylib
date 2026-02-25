a26ba0ea2f1410b6a3e0ac141f865185af683c04
Audrey Castillo <valax321@gmail.com>
2026-02-25 12:52:55 +1100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -5,7 +5,9 @@ $sdlVersion = '3.4.2'
 # We need to ignore homebrew entirely to be sure the output doesn't depend on anything that may be installed.
 # This also means we need to use Xcode instead of ninja.
 &cmake -S src/SDL3 -B build/SDL3 -G "Xcode" -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.13" -DCMAKE_IGNORE_PATH="/opt/homebrew/bin;/opt/homebrew/lib"
-&cmake --build build/SDL3 --config Release --target SDL3-shared
+
+# We disable code signing since we want to sign the entire finished app bundle ourselves later.
+&cmake --build build/SDL3 --config Release --target SDL3-shared -- CODE_SIGNING_ALLOWED=NO
 
 New-Item -ItemType "Directory" -Name "artifacts"
 Copy-Item "build/SDL3/Release/libSDL3.0.dylib" -Destination "$pwd\artifacts"
